dcba908cecadaeac19e363a566db6cb90d29f539
Roberto Lopez <roberto.lopez736146@gmail.com>
2024-11-25 02:36:23 +0000

    did some work in cotroller still need to work on it

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -10,6 +10,7 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.util.List;
+import java.util.logging.Handler;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -17,6 +18,13 @@ import java.util.List;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController(){
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -24,7 +32,14 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::registerHandler);
+        app.post("/login", this:: loginHandler);
+        app.post("/messages", this::postMessageHandler);
+        app.get("/messages", this::getMessagesHandler);
+        app.get("/messages/{message_id}", this::getMessageByMessageIDHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageByIDHandler);
+        app.patch("/messages/{message_id}", this::patchMessageByIDHandler);
+        app.get("/accounts/{account_id}/messages", this::getMessagesByAccountIDHandler);
 
         return app;
     }
@@ -33,9 +48,42 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    private void registerHandler(Context ctx) {
+        ctx.json("sample text");
     }
 
+    private void loginHandler(Context ctx) {
+        ctx.json("sample text");
+    }
 
+    private void postMessageHandler(Context ctx) throws JsonProcessingException{
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        Message addedMessage = messageService.newMessage(ctx.body(), message);
+        if(addedMessage!=null){
+            ctx.json(mapper.writeValueAsString(addedMessage));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+    private void getMessagesHandler(Context ctx) {
+        List<Message> messages = messageService.getAllMessages();
+        ctx.json(messages);
+    }
+
+    private void getMessageByMessageIDHandler(Context ctx) {
+        ctx.json("sample text");
+    }
+
+    private void deleteMessageByIDHandler(Context ctx) {
+        ctx.json("sample text");
+    }
+    private void patchMessageByIDHandler(Context ctx) {
+        ctx.json("sample text");
+    }
+
+    private void getMessagesByAccountIDHandler(Context ctx) {
+        ctx.json("sample text");
+    }
 }
